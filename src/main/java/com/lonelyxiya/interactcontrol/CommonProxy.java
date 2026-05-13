6ed61ea017569db0929e06ca108c859df8bdd533
Lonelyxiya <3438505487@qq.com>
2026-05-13 13:50:31 +0800

    112

---
+++ src/main/java/com/lonelyxiya/interactcontrol/CommonProxy.java
@@ -0,0 +1,16 @@
+package com.lonelyxiya.interactcontrol;
+
+public class CommonProxy {
+
+    public void preInit() {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void postInit() {
+
+    }
+}
--- src/main/java/com/lonelyxiya/interactcontrol/InteractControl.java
+++ src/main/java/com/lonelyxiya/interactcontrol/InteractControl.java
@@ -2,11 +2,31 @@ package com.lonelyxiya.interactcontrol;
 
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class InteractControl {
 
-    
+    @SidedProxy(clientSide = "com.lonelyxiya.interactcontrol.ClientProxy", serverSide = "com.lonelyxiya.interactcontrol.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init();
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit();
+    }
 
 }
